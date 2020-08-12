859e9e64515d38a8acf405e8f6aa03aec78ff288
jesseggoodier <jessegoodier@gmail.com>
2020-08-12 21:37:19 +0000

    initial load

--- docker-build.sh
+++
@@ -1,1 +0,0 @@
-docker build --build-arg CONTROLLER_URL=https://labcontroller.nginx.rocks:8443/1.4 --build-arg API_KEY='e5c24dd8b295c88c71b5114724c3f573' --tag=nginx-app-protect-with-controller-agent .
